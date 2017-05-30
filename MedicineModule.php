274cec2a78688acab5acdd7b53a6d337c525c1ea
Eugene Anufriev <eugene.anufriev@gmail.com>
2017-05-30 17:23:42 +0300

    add json

---
+++ MedicineModule.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace ut8ia\medicine;
+
+use Yii;
+use \yii\base\Module;
+
+class MedicineModule extends Module
+{
+
+    public function init()
+    {
+        parent::init();
+    }
+
+}
